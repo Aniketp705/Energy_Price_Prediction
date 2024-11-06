602cc665da7d303a3dce7814766caefba7ccdb02
Aniketp705 <aniket22217pawar@gmail.com>
2024-11-06 20:24:19 +0530

    enhance home.py with data preprocessing and file upload functionality for energy price prediction

--- home.py
+++ home.py
@@ -1,16 +1,41 @@
 import streamlit as st
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+import seaborn as sns
 
+
+def preprocess_data(data):
+    # data = data.dropna()
+    data = data.drop_duplicates()
+    data = data.drop(['generation hydro pumped storage aggregated','forecast wind offshore eday ahead'], axis=1)
+    data = data.drop(['time'], axis=1)
+    nan_percentages = round(data.isna().sum() / len(data) * 100, 2)
+    null_values= ['generation marine',
+                 'generation geothermal',
+                 'generation fossil peat',
+                 'generation wind offshore',
+                 'generation fossil oil shale',
+                 'generation fossil coal-derived gas']
+    data = data.drop(null_values, axis=1)
+
+
+    return data
 
 
 def app():
     st.title('Home')
     st.title('Welcome to the Energy Price Prediction App!')
-    st.write('Please enter the following features to predict the price of energy')
+    st.write('Please enter the CSV file to predict the energy price')
 
-    feature1 = st.text_input('Enter feature 1')
-    feature2 = st.text_input('Enter feature 2')
-    feature3 = st.text_input('Enter feature 3')
-    feature4 = st.text_input('Enter feature 4')
-    feature5 = st.text_input('Enter feature 5')
-
-    st.button('Predict')
+    uploaded_file = st.file_uploader("Choose a file")
+    if st.button('Predict'):
+        if uploaded_file is not None:
+            data = pd.read_csv(uploaded_file)
+            processed_data = preprocess_data(data)
+            st.write(processed_data)
+            plt.figure(figsize=(15, 10))
+            sns.histplot(processed_data, x='price actual')
+            st.pyplot(plt)
+        else:
+            st.error('Please upload a file')
