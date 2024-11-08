f2684bbbf7cc9f60ecdcc4da3b845d60fa55f22c
Aniketp705 <aniket22217pawar@gmail.com>
2024-11-08 20:16:35 +0530

    Delete app.py

--- app.py
+++
@@ -1,9 +0,0 @@
-import streamlit as st
-import pandas as pd
-import streamlit_option_menu as som
-import firebase_admin
-
-from firebase_admin import credentials
-from firebase_admin import auth
-
-
